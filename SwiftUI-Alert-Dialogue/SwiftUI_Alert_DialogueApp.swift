53112ffc08af3bc0f76e3eed5a07535abe96ab85
markumichaeloh@gmail.com <markumichaeloh@gmail.com>
2024-09-14 09:20:46 +0900

    Initial commit

---
+++ SwiftUI-Alert-Dialogue/SwiftUI_Alert_DialogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Alert_DialogueApp.swift
+//  SwiftUI-Alert-Dialogue
+//
+//  Created by Seungsub Oh on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Alert_DialogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
